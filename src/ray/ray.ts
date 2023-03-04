29228f7daad518099523f425b889e18a077308d0
Ariel <ariel.riveros@ing.uchile.cl>
2023-03-04 12:05:46 -0300

    refactor: ray has public members

--- src/ray/ray.ts
+++ src/ray/ray.ts
@@ -2,18 +2,12 @@ import { vec3 } from "gl-matrix";
 
 export default class Ray
 {
-    private _origin: vec3;
-    private _direction: vec3;
+    public origin: vec3;
+    public direction: vec3;
 
     constructor(origin?: vec3, direction?: vec3)
     {
-        this._origin = origin ? origin : vec3.create();
-        this._direction = direction ? direction : vec3.create();
+        this.origin = origin ? origin : vec3.create();
+        this.direction = direction ? direction : vec3.create();
     }
-
-    public get origin(): vec3 { return this._origin; }
-    public set origin(value: vec3) { this._origin = value; }
-
-    public get direction(): vec3 { return this._direction; }
-    public set direction(value: vec3) { this._direction = value; }
 }
